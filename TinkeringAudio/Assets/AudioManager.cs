fc27fc28708cc8795ca633a8b9b8b9cdabe22ae0
EugeneFalmouth <jsyrickis@gmail.com>
2019-11-26 07:52:00 +0000

    Committed the unity project into the repo

---
+++ TinkeringAudio/Assets/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClick()
+    {
+        int frequency = (int)GameObject.Find("SoundFrequency").GetComponent<Slider>().value;
+        audioSource.clip = CreateSound(frequency);
+        audioSource.Play();
+    }
+
+    private AudioClip CreateSound(int frequency)
+    {
+        int sampleDurationSecs = 5;
+        int sampleRate = 44100;
+        int sampleLength = sampleRate * sampleDurationSecs;
+        float maxValue = 1f / 4f;
+
+        var audioClip = AudioClip.Create("footsteps", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+        for (int i = 0; i < sampleLength; i++)
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+}
---
+++ TinkeringAudio/Assets/TextUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUpdate : MonoBehaviour
+{
+    public Text labelText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        labelText.text = GameObject.Find("SoundFrequency").GetComponent<Slider>().value.ToString() + " Hz";
+    }
+}
